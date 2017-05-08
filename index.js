f0acb0cb1eb88c925989c19403bfc44133fb11f8
liangweibin <liangweibin@baidu.com>
2017-05-08 18:05:19 +0800

    developing...

--- index.js
+++ index.js
@@ -8,18 +8,34 @@
  * each engineer has a duty to keep the code elegant
  */
 
-var getMusicData = require('./lib/getMusicData');
-var getMusicList = require('./lib/getMusicList');
 const util = require('util');
 
-/*getMusicData('./test_music/Sleep Away.mp3', function (metadata) {
+function testMusicData() {
+    var getMusicData = require('./lib/getMusicData');
 
-    // for debug
-    console.log(util.inspect(metadata, {showHidden: false, depth: null}));
-});*/
+    getMusicData('./test_music/Sleep Away.mp3', function (metadata) {
+        console.log(util.inspect(metadata, {showHidden: false, depth: null}));
+    });
+}
 
-getMusicList('./test_music/', function (list) {
+function testMusicList() {
+    var getMusicList = require('./lib/getMusicList');
 
-    // for debug
-    console.log(util.inspect(list, {showHidden: false, depth: null}));
-});
+    getMusicList('./test_music/', function (list) {
+        console.log(util.inspect(list, {showHidden: false, depth: null}));
+    });
+}
+
+function testMusicListToJsonFile() {
+    var updateMusicListFile = require('./lib/updateMusicListFile');
+
+    updateMusicListFile('./test_music/', function (err) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: false, depth: null}));
+        } else {
+            console.log('success!');
+        }
+    });
+}
+
+testMusicListToJsonFile();
